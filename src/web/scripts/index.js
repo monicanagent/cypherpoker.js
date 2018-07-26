d48b066fc9121a564b1f8e2761dccf710e99e872
Patrick Bay <monicanagent@users.noreply.github.com>
2018-07-26 01:36:23 -0400

    Added main CypherPoker.js interface script. Moved external JavaScript loading to index.js to ensure that dependencies are present. Updated EventDispatcher to include execution context for event listeners.

--- src/web/scripts/index.js
+++ src/web/scripts/index.js
@@ -1,88 +1,129 @@
-//dev URLs:
-var sessionURL = "ws://localhost:8090";
-var handshakeURL = "http://localhost:8080";
-console.log ("Starting crypto test...");
+/**
+* @file Main index JavaScript file responsible for loading additional
+* external scripts.
+*/
 
+/**
+* @property {String} _settingsURL The URL of the main settings JSON file.
+* @private
+*/
+const _settingsURL = "./scripts/settings.json";
+/**
+* @property {Boolean} _useCache Will force script-based loads to bypass
+* local browser caches if false.
+* @private
+*/
+const _useCache = false;
+/**
+* @property {CypherPoker} A reference to the main CypherPoker.JS instance.
+* @private
+*/
+var cypherpoker = null;
 
-// TEST 1: Asynchronous SRA cryptosystem example:
-var SRA = new SRACrypto(1);
-
-async function encryptionTest() {
-   var resultObj = await SRA.invoke("randomPrime", {bitLength:128, radix:16});
-   var prime = resultObj.data.result;
-   console.log ("New random prime: "+prime);
-
-   resultObj = await SRA.invoke("randomKeypair", {"prime":prime});
-   var keypair1 = resultObj.data.result.keypair;
-   console.log ("Keypair 1: "+JSON.stringify(keypair1));
-
-   resultObj = await SRA.invoke("randomKeypair", {"prime":prime});
-   var keypair2 = resultObj.data.result.keypair;
-   console.log ("Keypair 2: "+JSON.stringify(keypair2));
-
-   resultObj = await SRA.invoke("randomQuadResidues", {"prime":prime, "numValues":1})
-   var residue = resultObj.data.result[0];
-   console.log ("Quadratic residue mod prime: "+residue);
-
-   resultObj = await SRA.invoke("encrypt", {"value":residue, "keypair":keypair1});
-   var encrypted = resultObj.data.result;
-   console.log ("Encrypted value: "+encrypted);
-
-   resultObj = await SRA.invoke("encrypt", {"value":encrypted, "keypair":keypair2});
-   encrypted = resultObj.data.result;
-   console.log ("Double-encrypted value: "+encrypted);
-
-   resultObj = await SRA.invoke("decrypt", {"value":encrypted, "keypair":keypair2});
-   var decrypted = resultObj.data.result;
-   console.log ("Single-decrypted value: "+decrypted);
-
-   resultObj = await SRA.invoke("decrypt", {"value":decrypted, "keypair":keypair1});
-   decrypted = resultObj.data.result;
-
-   console.log ("Plaintext value: "+decrypted);
-   console.log ("Matches original QR? "+(decrypted == residue));
-}
-encryptionTest();
-
-// Asynchronous WebSocket Sessions using WSS.js example:
-
-async function startWebSocketSession(HURL, SURL) {
-   session = new WSS (HURL);
-   if (HURL.toLowerCase().trim().startsWith("http")) {
-      var websocket = await session.connect(SURL, true);
-   } else {
-      websocket = await session.connect(SURL, false);
+/**
+* @property {Array} _require Indexed array of required external scripts,
+* in the order that they must be loaded in.
+* @property {String} _require.url The URL of the external script to load.
+* @property {Function} [_require.onload] A function reference to invoke
+* when the script is finished loading.
+* @private
+*/
+const _require = [
+   {"url":"./scripts/libs/EventDispatcher.js"},
+   {"url":"./scripts/libs/EventPromise.js"},
+   {"url":"./scripts/libs/RPC.js"},
+   {"url":"./scripts/libs/TextEncoder.js"},
+   {"url":"./scripts/libs/WSS.js"},
+   {"url":"./scripts/libs/WorkerHost.js"},
+   {"url":"./scripts/libs/SRACrypto.js"},
+   {"url":"./scripts/CypherPoker.js",
+    "onload":() => {
+      //EventDispatcher and EventPromise must already exist here!
+      loadJSON(_settingsURL).onEventPromise("load").then(promise => {
+         if (promise.target.response != null) {
+            cypherpoker = new CypherPoker(promise.target.response);
+            cypherpoker.js = cypherpoker;
+            cp = cypherpoker;
+            cypherpoker.start().then(result => {
+               console.log ("CypherPoker.JS instance fully started and connected.");
+            }).catch(err => {
+               console.error ("CypherPoker.JS instance couldn't connect to peer-to-peer network:\n"+err.stack);
+            });
+         } else {
+            alert (`Settings data (${_settingsURL}) not loaded or parsed.`);
+            throw (new Error(`Settings data (${_settingsURL}) not loaded or parsed.`));
+         }
+      });
+    }
    }
-   return (session);
+]
+
+/**
+* Loads an external JavaScript file by adding a <script> tag to the
+* DOM's <head> tag.
+*
+* @param {String} scriptURL The URL of the script to load and parse.
+* @private
+*/
+function loadJavaScript(scriptURL) {
+   var script = document.createElement("script");
+   script.setAttribute("type", "text/javascript");
+   script.setAttribute("language", "text/JavaScript");
+   if (_useCache == false) {
+      //force new script
+      scriptURL = scriptURL + "?" + String(Math.random()).split("0.")[1];
+   }
+   script.setAttribute("src", scriptURL);
+   script.addEventListener ("load", onLoadJavaScript);
+   document.getElementsByTagName("head")[0].appendChild(script);
 }
 
-startWebSocketSession(handshakeURL, sessionURL).then(session => {
-   session.addEventListener("message", onSessionMessage);
-   session.addEventListener("close", onSessionClose);
-   session.addEventListener("peerconnect", onPeerConnect);
-   session.addEventListener("peerdisconnect", onPeerDisconnect);
-   console.log ("Successfully established WebSocket Session!");
-}).catch (err => {
-   console.error ("Couldn't establish WebSocket Session:\n"+err);
-})
-
-function onSessionMessage(event) {
-   console.log ("onsessionmessage: "+JSON.stringify(event.data));
-   var resultObj = event.data.result;
-   alert ("Got "+resultObj.type+" message: "+resultObj.data +"\n\nFrom: "+resultObj.from);
+/**
+* Event handler invoked when an external JavaScript file has completed loading.
+* The next file in the {@link _requires} array is automatically loaded.
+*
+* @param {Event} event A standard DOM event object.
+* @private
+*/
+function onLoadJavaScript(event) {
+   var loadedObj = _require.shift(); //important! -- remove current element from array
+   var loadedURL = loadedObj.url;
+   var loadedTimeStamp = new Date(event.timeStamp);
+   console.log (`"${loadedURL}" loaded at ${loadedTimeStamp.getSeconds()}s ${loadedTimeStamp.getMilliseconds()}ms`);
+   if (_require.length > 0) {
+      loadJavaScript (_require[0].url);
+   } else {
+      console.log (`All scripts loaded in ${loadedTimeStamp.getSeconds()}s ${loadedTimeStamp.getMilliseconds()}ms`);
+   }
+   if (typeof loadedObj["onload"] == "function") {
+      loadedObj.onload();
+   }
 }
 
-function onSessionClose(event) {
-   //may be closed by server, network connectivity issues, etc.
-   console.log ("WebSocket Session has been externaly terminated.");
+/**
+* Loads an external JSON data file using XMLHTTPRequest.
+*
+* @param {String} jsonURL The URL of the JSON data file to load and parse.
+* @private
+*/
+function loadJSON(jsonURL) {
+   var xhr = new XMLHttpRequest();
+   if (_useCache == false) {
+      //force new data load
+      jsonURL = jsonURL + "?" + String(Math.random()).split("0.")[1];
+   }
+   xhr.open("GET", jsonURL);
+   xhr.overrideMimeType("application/json");
+   xhr.responseType = "json";
+   xhr.send();
+   return (xhr);
 }
 
-function onPeerConnect(event) {
-   console.log ("A new peer has connected: "+event.data.result.connect);
+/**
+* Main page load handler; invokes {@link loadJavaScript} with the first
+* JavaScript file found in the {@link _requires} array.
+* @private
+*/
+onload = function () {
+   loadJavaScript (_require[0].url);
 }
-
-function onPeerDisconnect(event) {
-   console.log ("A peer has disconnected: "+event.data.result.disconnect);
-}
-
-console.log ("index.js loaded and running");
--- src/web/scripts/libs/EventDispatcher.js
+++ src/web/scripts/libs/EventDispatcher.js
@@ -1,5 +1,5 @@
 /**
-* @file Extensible class intended to mimic standard DOM event dispatchers.
+* @file Extensible class intended to (nearly) mimic standard DOM event dispatchers.
 */
 /**
 * @class Extend this class to enable standard event dispatcher functionality
@@ -22,34 +22,74 @@
 class EventDispatcher {
 
    constructor () {
-       this._registrations= {};
+       this._registrations = new Object();
    }
 
-   getListeners (type, useCapture) {
-       var captype= (useCapture? '1' : '0')+type;
-       if (!(captype in this._registrations))
-           this._registrations[captype]= [];
-       return this._registrations[captype];
+   /**
+   * Returns all registered listeners for a specific event type.
+   *
+   * @param {String} type The event type to return registered listeners for.
+   *
+   * @return {Array} A list of registered event listener objects for the specific event.
+   * Each object contains a <code>listener</code> function reference and an
+   * execution <code>context</code> reference.
+   */
+   getListeners (type) {
+       if ((this._registrations[type] == undefined) || (this._registrations[type] == null)) {
+          this._registrations[type] = new Array();
+       }
+       return (this._registrations[type]);
    }
 
-   addEventListener (type, listener, useCapture) {
-       var listeners= this.getListeners(type, useCapture);
-       var ix= listeners.indexOf(listener);
-       if (ix===-1)
-           listeners.push(listener);
+   /**
+   * Registers a new event listener with the extending class instance.
+   *
+   * @param {String} type The event type to register.
+   * @param {Function} listener The listening function to invoke on the event.
+   * @param {Object} [context=null] Unlike the traditional "addEventListener" parameter, this is
+   * the context or scope in which to invoke the listening function (since we can't use capture phases).
+   * If null, the listener is invoked in the context of the EventDispatcher instance.
+   */
+   addEventListener (type, listener, eventContext = null) {
+       var listeners= this.getListeners(type);
+       for (var count=0; count < listeners.length; count++) {
+          if (listeners[count].listener == listener) {
+             return;
+          }
+       }
+       var listenerObj = new Object();
+       listenerObj.listener = listener;
+       listenerObj.context = eventContext;
+       listeners.push(listenerObj);
    }
 
-   removeEventListener (type, listener, useCapture) {
-       var listeners= this.getListeners(type, useCapture);
-       var ix= listeners.indexOf(listener);
-       if (ix!==-1)
-           listeners.splice(ix, 1);
+   /**
+   * Removes an event listener from the extending instance. The standard capture phase
+   * parameter is ignored.
+   *
+   * @param {String} type The event type to remove the function from.
+   * @param {Function} listener The listening function to remove.
+   *
+   * @augments EventDispatcher
+   */
+   removeEventListener (type, listener) {
+       var listeners= this.getListeners(type);
+       for (var count=0; count < listeners.length; count++) {
+          if (listeners[count].listener == listener) {
+             listeners.splice(count, 1);
+             return;
+          }
+       }
    }
 
    dispatchEvent (evt) {
-       var listeners = this.getListeners(evt.type, false).slice();
-       for (var i= 0; i<listeners.length; i++) {
-         listeners[i].call(this, evt);
+       var listeners = this.getListeners(evt.type).slice();
+       for (var i= 0; i < listeners.length; i++) {
+         if (listeners[i].context != null) {
+            listeners[i].listener.call(listeners[i].context, evt);
+         } else {
+            listeners[i].listener.call(this, evt);
+         }
        }
        return !evt.defaultPrevented;
    }
