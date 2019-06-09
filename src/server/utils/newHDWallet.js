37f4f631501c26853c92471f76b526a46a5689cc
Patrick Bay <monicanagent@users.noreply.github.com>
2019-06-08 20:02:49 -0400

    Added v0.5.0-alpha.15: Added Bitcoin Cash wallet support to newHDWallet utility.

--- src/server/utils/newHDWallet.js
+++ src/server/utils/newHDWallet.js
@@ -8,6 +8,7 @@
 const bip39 = require('bip39');
 const bip32 = require('bip32');
 const bitcoin = require('bitcoinjs-lib');
+const bitcoreCash = require('bitcore-lib-cash');
 
 /**
 * @private
@@ -57,20 +58,44 @@ function makeHDWallet(privKey, network=null) {
 }
 
 /**
-* @private
+* Returns the address of a Bitcoin wallet object.
+*
+* @param {Object} walletObj A Bitcoin wallet data object.
+* @param {String} [network="main"] The sub-network for which to get the address.
+* Either "main" (default), "test", "regtest", or any common BCH variant of these.
+* @param {Boolean} [cashAddress=false] If true, a Bitcoin Cash address is returned instead
+* of a standard (legacy) Bitcoin Base58 address.
+*
 */
-function getAddress (walletObj, network) {
-  return bitcoin.payments.p2pkh({pubkey:walletObj.publicKey, network}).address;
+function getAddress(walletObj, network=null, cashAddress=false) {
+   if (network == null) {
+      network = bitcoin.networks.mainnet;
+   }
+   if (cashAddress == true) {
+      var publicKey = bitcoreCash.PublicKey(walletObj.publicKey);
+      var address = new bitcoreCash.Address(publicKey, network);
+      return (address.toString());
+   } else {
+      return (bitcoin.payments.p2pkh({pubkey:walletObj.publicKey, network:network}).address);
+   }
 }
+
+if ((process.argv[2] == "test") || (process.argv[2]=="test3")) {
+   process.argv[2] = "testnet";
+}
+
 
 var testnet = false;
 if (process.argv[2] != "testnet") {
+   console.log ("Bitcoin");
+   console.log ("-------");
    var wallet = newHDWallet(); //create a mainnet top-level HD node
 } else {
+   console.log ("Testnet Bitcoin");
+   console.log ("---------------");
    testnet = true;
    wallet = newHDWallet(bitcoin.networks.testnet); //create a testnet top-level HD node
 }
-
 let pubKey = wallet.neutered().toBase58();//public key in "xpub"
 let pubKeyHex = wallet.neutered().publicKey.toString("hex"); //public key in hex
 let privKey = wallet.toBase58(); //private key in "xprv"
@@ -98,6 +123,45 @@ if (!testnet) {
    console.log("Inital derived address (m/0/1): "+getAddress(childBIP32_1, bitcoin.networks.testnet));
 }
 console.log(" ");
+
+if (process.argv[2] != "testnet") {
+   console.log ("Bitcoin Cash");
+   console.log ("------------");
+   var wallet = newHDWallet(); //create a mainnet top-level HD node
+} else {
+   console.log ("Testnet Bitcoin Cash");
+   console.log ("--------------------");
+   testnet = true;
+   wallet = newHDWallet(bitcoin.networks.testnet); //create a testnet top-level HD node
+}
+
+pubKey = wallet.neutered().toBase58();//public key in "xpub"
+pubKeyHex = wallet.neutered().publicKey.toString("hex"); //public key in hex
+privKey = wallet.toBase58(); //private key in "xprv"
+privKeyHex = wallet.privateKey.toString("hex"); //private key in hex
+wif = wallet.toWIF();
+childBIP32_0 = wallet.derivePath("m/0/0");
+childBIP32_1 = wallet.derivePath("m/0/1");
+if (!testnet) {
+   console.log("Wallet root address: "+getAddress(wallet, bitcoreCash.Networks.livenet, true));
+   console.log("Wallet \"xpub\": "+pubKey);
+   console.log("Wallet \"xprv\": "+privKey);
+   console.log("Wallet Import Format (WIF): "+wif);
+   console.log("Wallet public key (hex): "+pubKeyHex);
+   console.log("Wallet private key (hex): "+privKeyHex);
+   console.log("Inital derived address (m/0/0): "+getAddress(childBIP32_0, bitcoreCash.Networks.livenet, true));
+   console.log("Inital derived address (m/0/1): "+getAddress(childBIP32_1, bitcoreCash.Networks.livenet, true));
+} else {
+   console.log("Testnet wallet root address: "+getAddress(wallet, bitcoreCash.Networks.testnet, true));
+   console.log("Testnet wallet \"tpub\": "+pubKey);
+   console.log("Testnet wallet \"tprv\": "+privKey);
+   console.log("Testnet Wallet Import Format (WIF): "+wif);
+   console.log("Testnet wallet public key (hex): "+pubKeyHex);
+   console.log("Testnet wallet private key (hex): "+privKeyHex);
+   console.log("Inital derived address (m/0/0): "+getAddress(childBIP32_0, bitcoreCash.Networks.testnet, true));
+   console.log("Inital derived address (m/0/1): "+getAddress(childBIP32_1, bitcoreCash.Networks.testnet, true));
+}
+console.log ("");
 
 // Some other examples:
 
